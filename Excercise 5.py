b186b7fdb4cd17fb4412f450d5ed66d20008b721
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:27:43 +0200

    Update Excercise 5.py

--- Excercise 5.py
+++ Excercise 5.py
@@ -1,15 +1,23 @@
 # 5 Excercise: Draw a icosceles triangle
 
-size = int(input("Please enter the number of rows: "))
+#Defining function
+def icos_triangle():
 
-m = (2 * size) - 2
-for i in range(0, size):
+    # Prompt user to enter input
+    size = int(input("Please enter the number of rows: "))
 
-    for j in range(0, m):
-        print(end=" ")
-    m = m - 1 #decrementing m after each loop
-    for j in range(0, i + 1):
-        #printinh full riangle pyramid using hash
-        print("# ", end='')
-    print(" ")
+    # For loop
+    m = (2 * size) - 2
+    for i in range(0, size):
+
+        for j in range(0, m):
+            print(end=" ")
+        m = m - 1    # decrementing m after each loop
+        for j in range(0, i + 1):
+            # printinh full triangle pyramid using hash
+            print("# ", end='')
+        print(" ")
+
+# Printing function
+print(icos_triangle())
 
