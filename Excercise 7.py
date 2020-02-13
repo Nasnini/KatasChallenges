cde787230efa7cacb8277bc41c4d33bc7bbd51ce
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:30:19 +0200

    Update Excercise 7.py

--- Excercise 7.py
+++ Excercise 7.py
@@ -1,10 +1,11 @@
 # 7 Excercise: Combine two lists/arrays
 
+#function taking 2 parameters
 def combineList(list1, list2): 
-    return [sub[item] for item in range(len(list2)) 
+    return [sub[item] for item in range(len(list2))  #altenating between list1 and list2
                       for sub in [list1, list2]] 
       
 # Driver code 
 list1 = [11, 22, 33] 
 list2 = [1, 2, 3] 
-print(combineList(list1, list2)) 
+print(combineList(list1, list2))     #Print the function with the arguements parse in list1 and list2
