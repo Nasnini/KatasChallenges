9f17e20d5f1838e46fff824e896a3844bcf2a186
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:27:03 +0200

    Update Excercise 4.py

--- Excercise 4.py
+++ Excercise 4.py
@@ -1,13 +1,19 @@
 # 4 Excercise: Draw a right handed triangle
-#def right_triangle():
-size = int(input("Please enter the number of rows: "))
 
-print("Right Angled Triangle: ")
-i = 1
-while(i <= size):
-    j = 1
-    while(j <= i):
-        print('#', end = '  ')
-        j = j + 1
-    i = i + 1
-    print()
+# Defining function
+def right_triangle():
+
+    size = int(input("Please enter the number of rows: "))
+
+    print("Right Angled Triangle: ")
+    # While loop to print function
+    i = 1
+    while(i <= size):
+        j = 1
+        while(j <= i):
+            print('#', end = '  ')
+            j = j + 1
+            i = i + 1
+
+# Printing function
+print(right_triangle())
