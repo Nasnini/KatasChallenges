2f555e122d14b21f47bea9c9ff37e88ff17295bb
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:31:10 +0200

    Update Excercise 8.py

--- Excercise 8.py
+++ Excercise 8.py
@@ -1,6 +1,6 @@
 # 8 Excercise: Frame some text
-#doesn't run   
 
+# function taking 2 parameters  
 def print_in_a_frame(words, borderchar = '*'):
     size = max(len(word) for word in words)
     print(borderchar * (size + 4))
@@ -8,4 +8,5 @@ def print_in_a_frame(words, borderchar = '*'):
         print('{bc} {:<{}} {bc}'.format(word, size, bc = borderchar))
     print(borderchar * (size + 4))
 
->>> print_in_a_frame("Write", "good", "code", "every", "day")
+# print the list
+print_in_a_frame(["Write", "good", "code", "every", "day"])
