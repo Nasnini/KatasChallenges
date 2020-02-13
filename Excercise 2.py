777bdc5b06f5fa87bf6e8a9900e802530fbeed96
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:25:46 +0200

    Update Excercise 2.py

--- Excercise 2.py
+++ Excercise 2.py
@@ -1,8 +1,12 @@
 # 2. Exercise: check if a number is even
 
-num = int(input("Enter a number: "))
-mod = num % 2
-if mod > 0:
-	print("This is an odd number.")
-else:
-	print("This is an even number.")
+# prompts user to enter a number and store in 'num' variable
+num=int(input("Enter a number for check odd or even: "))
+def find_even_odd(num):  #parsing num as the arguement for even_odd function
+        
+    # Check is num is even or odd
+    if(num%2==0):   #condition to check
+        print(num," Is an even")
+    else:
+        print(num," is an odd")
+    find_even_odd(num);     # function call
