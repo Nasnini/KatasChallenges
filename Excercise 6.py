5f3df78adf35af06e7d68c7a1c24381bd25f4f2d
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-02-13 14:28:31 +0200

    Update Excercise 6.py

--- Excercise 6.py
+++ Excercise 6.py
@@ -1,8 +1,9 @@
 # 6 Excercise: Find the longest string
-#Call a function
 
+# Defigning the function
 def longest():
 
+# List to check which string is the longest
 test_list = ["the","quick","brown", "fox", "ate", "my", "chickens"] 
   
 # Printing original list  
@@ -14,7 +15,7 @@ max_len = -1
 for ele in test_list: 
     if len(ele) > max_len: 
         max_len = len(ele) 
-        res = ele 
+        result = ele 
   
 # printing result 
-print("The longest string in the list is: " + res)
+print("The longest string in the list is: " + result)
